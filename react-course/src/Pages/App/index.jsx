7a9f1bb0977261ce7205e184f644accb90c8971f
josue3002 <josuegullo163@gmail.com>
2024-09-02 19:43:54 -0400

    agregando routes

--- react-course/src/Pages/App/index.jsx
+++ react-course/src/Pages/App/index.jsx
@@ -1,3 +1,5 @@
+
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -6,17 +8,24 @@ import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sign-in', element: <SignIn /> },
+  ])
 
+  return routes
+}
+
+const App = () => {
   return (
-      <div className='bg-red-100'>
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SignIn />
-      </div>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
   )
 }
 
