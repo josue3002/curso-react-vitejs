7858c96e957597774d3ea149d5be499333863d35
josue3002 <josuegullo163@gmail.com>
2024-09-11 19:06:29 -0400

    agregando la card

--- react-course/src/Pages/Home/index.jsx
+++ react-course/src/Pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import Layout from '../../components/Layout'
+import Card from '../../components/Card'
 
 function home() {
     return (
         <Layout>
           Home
+          <Card />
         </Layout>
     )
   }
