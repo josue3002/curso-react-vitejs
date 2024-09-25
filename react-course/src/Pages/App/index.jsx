fbc4715af17b94096b818d6e44ef9e1d6db6b0e9
josue3002 <josuegullo163@gmail.com>
2024-09-25 19:10:46 -0400

    algregando el provider

--- react-course/src/Pages/App/index.jsx
+++ react-course/src/Pages/App/index.jsx
@@ -1,5 +1,6 @@
 
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider } from '../../Context'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -24,10 +25,12 @@ const AppRoutes = () => {
 
 const App = () => {
   return (
+    <ShoppingCartProvider>
       <BrowserRouter>
         <AppRoutes />
         <Navbar />
       </BrowserRouter>
+    </ShoppingCartProvider>   
   )
 }
 
